9717838ca7c8ce2c4eb23785a0401ff7ae02c3b1
Merus23 <mateus.satira@gmail.com>
2025-01-12 13:49:36 -0300

    init:initial commit

---
+++ src/main/java/com/example/jwt_authentication_model/JwtAuthenticationModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_authentication_model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwt_authentication_model/JwtAuthenticationModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_authentication_model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
