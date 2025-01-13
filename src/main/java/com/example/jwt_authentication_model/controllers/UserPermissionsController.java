843fe51b4ba729d15b32551e101862ab5f2db3d3
Merus23 <mateus.satira@gmail.com>
2025-01-13 12:34:34 -0300

    refactor: add the rights http code responses

--- src/main/java/com/example/jwt_authentication_model/controllers/UserPermissionsController.java
+++ src/main/java/com/example/jwt_authentication_model/controllers/UserPermissionsController.java
@@ -1,9 +1,12 @@
 package com.example.jwt_authentication_model.controllers;
 
 import com.example.jwt_authentication_model.dtos.UserPermissions.UserPermissionCreateDTO;
+import com.example.jwt_authentication_model.models.User;
 import com.example.jwt_authentication_model.models.UserPermission;
 import com.example.jwt_authentication_model.services.UserPermissionsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,18 +19,18 @@ public class UserPermissionsController {
     private UserPermissionsService userPermissionsService;
 
     @GetMapping(value = "/")
-    public List<UserPermission> findAllPermissions() {
-        return userPermissionsService.findAll();
+    public ResponseEntity<List<UserPermission>> findAllPermissions() {
+        return ResponseEntity.ok(userPermissionsService.findAll());
     }
 
     @GetMapping("/{id}")
-    public UserPermission findById(@PathVariable(value = "id") Long id) {
-        return userPermissionsService.findById(id);
+    public ResponseEntity<UserPermission> findById(@PathVariable(value = "id") Long id) {
+        return ResponseEntity.ok(userPermissionsService.findById(id));
     }
 
     @PostMapping(value = "/")
-    public UserPermission createPermission(@RequestBody UserPermissionCreateDTO permission) {
-        return userPermissionsService.create(permission);
+    public ResponseEntity<UserPermission> createPermission(@RequestBody UserPermissionCreateDTO permission) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userPermissionsService.create(permission));
     }
 
     @PutMapping(value = "/")
@@ -36,7 +39,8 @@ public class UserPermissionsController {
     }
 
     @DeleteMapping("/{id}")
-    public void deletePermission(@PathVariable(value = "id") Long id) {
+    public ResponseEntity deletePermission(@PathVariable(value = "id") Long id) {
         userPermissionsService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
