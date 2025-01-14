cfd56d5a9613b60ff744a890607b5917fa8dcb64
Merus23 <mateus.satira@gmail.com>
2025-01-14 16:06:44 -0300

    fix: remove some properties from annotations

--- src/main/java/com/example/jwt_authentication_model/JwtAuthenticationModelApplication.java
+++ src/main/java/com/example/jwt_authentication_model/JwtAuthenticationModelApplication.java
@@ -2,8 +2,10 @@ package com.example.jwt_authentication_model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.jwt_authentication_model")
 public class JwtAuthenticationModelApplication {
 
 	public static void main(String[] args) {
