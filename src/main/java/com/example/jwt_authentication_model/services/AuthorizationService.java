e7209a4173ea4254d3099ac97639e0a403011ed1
Merus23 <mateus.satira@gmail.com>
2025-01-20 16:10:13 -0300

    feat: add the AuthorizationService that implements the UserDetailsService

--- src/main/java/com/example/jwt_authentication_model/services/AuthorizationService.java
+++ src/main/java/com/example/jwt_authentication_model/services/AuthorizationService.java
@@ -1,4 +1,23 @@
 package com.example.jwt_authentication_model.services;
 
-public class AuthorizationService {
+import com.example.jwt_authentication_model.repositoties.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("loadUserByUsername: User not found"));
+    }
 }
