98e31a956886b538dd5bb53171e6c3106c218a5c
Merus23 <mateus.satira@gmail.com>
2025-01-24 19:10:47 -0300

    chore: rename class

--- src/main/java/com/example/jwt_authentication_model/infra/security/AuthorizationConfiguration.java
+++ src/main/java/com/example/jwt_authentication_model/infra/security/AuthorizationConfiguration.java
@@ -7,11 +7,11 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthorizationConfigiguration implements UserDetailsService {
+public class AuthorizationConfiguration implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public AuthorizationConfigiguration(UserRepository userRepository) {
+    public AuthorizationConfiguration(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
