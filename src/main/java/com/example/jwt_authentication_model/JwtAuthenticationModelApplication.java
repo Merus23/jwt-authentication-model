2276dcea2a94c40e35d1262a6469f5478ed5f8cc
Merus23 <mateus.satira@gmail.com>
2025-01-24 18:14:01 -0300

    chore: remove useless lines

--- src/main/java/com/example/jwt_authentication_model/JwtAuthenticationModelApplication.java
+++ src/main/java/com/example/jwt_authentication_model/JwtAuthenticationModelApplication.java
@@ -1,10 +1,8 @@
 package com.example.jwt_authentication_model;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.flywaydb.core.Flyway;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.jwt_authentication_model")
