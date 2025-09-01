62a69fb4622469ce6752933618059a439a064154
Merus23 <mateus.satira@gmail.com>
2025-09-01 10:35:03 -0300

    test: add tests without the implementation. This is a model project, test implementation required a business rules

--- src/test/java/com/example/jwt_authentication_model/services/UserServiceTest.java
+++ src/test/java/com/example/jwt_authentication_model/services/UserServiceTest.java
@@ -1,11 +1,29 @@
 package com.example.jwt_authentication_model.services;
 
+import com.example.jwt_authentication_model.models.User;
+import com.example.jwt_authentication_model.models.UserPermission;
+import com.example.jwt_authentication_model.repositoties.UserPermissionsRepository;
+import com.example.jwt_authentication_model.repositoties.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.openMocks;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserPermissionsRepository userPermissionsRepository;
+
+    @InjectMocks
+    private UserService userService;
+
 
     @BeforeEach
     void setUp() {
